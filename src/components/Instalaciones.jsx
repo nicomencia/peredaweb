87c288571ae2aa127a23894623726c8e61c1d5ac
nicomencia <47212402+nicomencia@users.noreply.github.com>
2026-01-28 13:20:23 +0100

    Updated package-lock.json

--- src/components/Instalaciones.jsx
+++ src/components/Instalaciones.jsx
@@ -1,4 +1,14 @@
 import './Instalaciones.css';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
 
 const stores = [
   {
@@ -111,14 +121,24 @@ export default function Instalaciones() {
                 </div>
               </div>
               <div className="store-map">
-                <iframe
-                  src={`https://www.openstreetmap.org/export/embed.html?bbox=${store.lon - 0.01},${store.lat - 0.01},${store.lon + 0.01},${store.lat + 0.01}&layer=mapnik&marker=${store.lat},${store.lon}`}
-                  width="100%"
-                  height="100%"
-                  style={{ border: 0 }}
-                  loading="lazy"
-                  title={`Mapa de ${store.name}`}
-                ></iframe>
+                <MapContainer
+                  center={[store.lat, store.lon]}
+                  zoom={15}
+                  style={{ width: '100%', height: '100%' }}
+                  scrollWheelZoom={false}
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={[store.lat, store.lon]}>
+                    <Popup>
+                      <strong>{store.name}</strong><br />
+                      {store.address}<br />
+                      {store.postalCode}
+                    </Popup>
+                  </Marker>
+                </MapContainer>
               </div>
             </div>
           ))}
